b00c9146741f467143480cef24f709538dff4843
programmer <omkarsk98@gmail.com>
2019-03-13 16:41:25 +0530

    removed doc file

--- ReAdjustReadme.py
+++
@@ -1,9 +0,0 @@
-with open('README.md') as f:
-    lines = f.readlines()
-print(lines)
-outputFile = ""
-for i in range(len(lines)):
-    outputFile = "<br/>"+outputFile + lines[i]
-print(outputFile)  
-newFile = open("README.md","w")
-newFile.write(outputFile)
